0615d2adfeb8e239371f86d692770ad29b69f8fc
aMehdipour <87405696+aMehdipour@users.noreply.github.com>
2021-08-29 14:08:56 -0700

    Fixed repeating quotes, authors, and tags.
    Fixed next page issue thanks to Amal.

--- xpath_to_scrape.py
+++ xpath_to_scrape.py
@@ -12,16 +12,17 @@ class QuotesSpider(scrapy.Spider):
         start_urls = [
             'http://quotes.toscrape.com/page/1/',
         ]
-
+        
         def parse(self, response):
-            for quote in response.xpath("//div[@class = 'quote']"):
+           
+            for quote in response.xpath("//div[@class and contains(concat(' ', normalize-space(@class), ' '), ' quote ')]"):
                 yield {
-                    'text': quote.xpath("//span[@class = 'text']/text()").get(),
-                    'author': quote.xpath("//small[@class = 'author']/text()").get(),
-                    'tags': quote.xpath("//div[@class = 'tags']/a[@class = 'tag']/text()").getall(),
+                    'text': quote.xpath("span[@class = 'text']/text()").get(),
+                    'author': quote.xpath("descendant-or-self::small[@class and contains(concat(' ', normalize-space(@class), ' '), ' author ')]/text()").get(),
+                    'tags': quote.xpath("div[@class = 'tags']/a[@class = 'tag']/text()").getall(),
                 }
-
-            next_page = response.xpath("//ul/li[@class = 'next']/a[@attr = 'href']").get()
+               
+            next_page = response.xpath("//li[@class = 'next']/a/@href").get()
             if next_page is not None:
                 next_page = response.urljoin(next_page)
                 yield scrapy.Request(next_page, callback=self.parse)
